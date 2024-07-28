772bfcbb685b721cccf44a874778b3e22c48a987
maia <maiam.unc@gmail.com>
2024-07-28 12:04:50 -0400

    debugging supabase login

--- app/login/supabase/route.js
+++ app/login/supabase/route.js
@@ -7,6 +7,7 @@ export async function GET(request) {
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/'
+  console.log("@/login/supabase, code:", code)
 
   if (code) {
     const cookieStore = cookies()
